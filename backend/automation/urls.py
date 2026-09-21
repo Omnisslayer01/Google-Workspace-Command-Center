8188e2f09e84c8cfaf4f4bc89a534e9d551a63c8
krish8924 <chaudharikrushna885@gmail.com>
2026-09-22 01:15:43 +0530

    BE2: Complete Task 4 - Execution Engine

--- backend/automation/urls.py
+++ backend/automation/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from .views import AutomationCreateView
+from .views import AutomationRunView
 
 urlpatterns = [
     path("", AutomationCreateView.as_view(), name="automation-create"),
+    path("<int:automation_id>/run/", AutomationRunView.as_view(), name="automation-run"),
 ]
